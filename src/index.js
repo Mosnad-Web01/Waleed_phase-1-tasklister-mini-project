e8745e6d60a9f5f76f0e057ec899efc4ecaa07b1
waleed7333 <waleed733150@gmail.com>
2024-08-19 13:29:20 +0300

    update JS file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document
+    .getElementById("create-task-form")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+
+      const taskDescription = document.getElementById(
+        "new-task-description"
+      ).value;
+
+      const li = document.createElement("li");
+      li.textContent = taskDescription;
+
+      document.getElementById("tasks").appendChild(li);
+
+      document.getElementById("new-task-description").value = "";
+    });
 });
